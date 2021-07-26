97b5832e57f44fae7014ef538ab3921005b47a3a
Matheus Lima <matheus.al1998@gmail.com>
2021-07-26 16:17:39 -0300

    [FEATURE] Criando repositorio

---
+++ Livros/Classes/LivroRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livros.Interfaces;
+
+namespace Livros.Classes
+{
+    public class LivroRepositorio : IRepositorio<Livro>
+    {
+
+        private List<Livro> listaLivro = new List<Livro>();
+
+        public List<Livro> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Livro RetornaPorId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Livro entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualiza(Guid id, Livro entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Livros/Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId();
+        void Insere(T entidade);
+        void Exclui(Guid id);
+        void Atualiza(Guid id, T entidade);
+        Guid ProximoId();
+    }
+}
