1c20be48c731032af2abfe337cad5d8a211beeb5
Matheus Lima <matheus.al1998@gmail.com>
2021-07-26 16:08:16 -0300

    [FEATURE] Criando estrutura e classes da aplicação

---
+++ Livros/Classes/Livro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livros.Enum;
+
+namespace Livros.Classes
+{
+    public class Livro : EntidadeBase
+    {
+
+        #region Propriedades
+
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private string Autor { get; set; }
+        private int AnoPublicacao { get; set; }
+        #endregion
+
+        #region Construtor
+        public Livro(Guid id, Genero genero, string titulo, string descricao, string autor, int anoPublicacao)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Autor = autor;
+            AnoPublicacao = anoPublicacao;
+        }
+
+        #endregion
+
+        #region Metodos
+        public override string ToString()
+        {
+            var retorno = "";
+
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Autor: " + this.Autor + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de publicação: " + this.AnoPublicacao + Environment.NewLine;
+
+            return retorno;
+        }
+
+        public string RetornaTItulo()
+        {
+            return this.Titulo;
+        }
+
+        public Guid RetornaId()
+        {
+            return this.Id;
+        }
+        #endregion
+    }
+}
---
+++ Livros/Enum/Genero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros.Enum
+{
+    public  enum  Genero
+    {
+        Ficcao = 1,
+        Religioso = 2,
+        Romance = 3, 
+        Classico = 4,
+        Biografico = 5
+    }
+}
